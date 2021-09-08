e73b36050e27879b5a370231c09ad8db92cf7d2d
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-09-08 16:29:32 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,12 @@
-#https://learn.chef.io/modules/learn-the-basics/ubuntu/bring-your-own-system/configure-a-package-and-service#/
-apt_update 'Update the apt cache daily' do
-  frequency 86_400
-  action :periodic
+package 'httpd' do
+  action :install
 end
-
-package 'apache2'
-
-service 'apache2' do
-  supports status: true
-  action [:enable, :start]
+ 
+file '/var/www/html/index.html' do
+  content "Ktexperts is a knowledge sharing platform"
+  action :create
 end
-
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
+ 
+service 'httpd' do
+  action [ :enable, :start ]
 end
